0d675759ea7d168440882b0207b5bb247b3a8c48
Sander_N <sandernommik@gmail.com>
2021-06-08 12:38:01 +0300

    Structure css, add pop-up dialog and POST request for vote
    See: #10

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import Option from './components/Option';
-import './style.css';
-import cat from './cat.png';
-import dog from './dog.png';
+import './style.css'
+import './buttons.css'
+import './options.css'
+import './dialog.css'
+import './animations.css'
+import optionOne from './cat.png';
+import optionTwo from './dog.png';
 
 const firstOption = "Cats";
 const secondOption = "Dogs";
-const firstOptionElement = <Option option={firstOption} src={cat} alt={"First option"}/>;
-const secondOptionElement = <Option option={secondOption} src={dog} alt={"Second option"}/>;
-const options = <div id="options">
+const firstOptionElement = <Option option={firstOption} src={optionOne} alt={"First option"}/>;
+const secondOptionElement = <Option option={secondOption} src={optionTwo} alt={"Second option"}/>;
+const options = <div className="options">
 		{firstOptionElement}
 		{secondOptionElement}
 </div>
@@ -19,7 +23,6 @@ function App() {
 			<h1 className="vote-subject">{firstOption} vs. {secondOption}</h1>
 			<h1 className="">Pick Your Favourite</h1>
 			{options}
-			<button type="submit" className="vote-button">Submit Your Vote</button>
 		</div>
 	);
 }
--- src/components/Option.js
+++ src/components/Option.js
@@ -4,22 +4,51 @@ export default class Option extends React.Component {
 	constructor(props) {
 		super(props);
 		this.state = {
-			toggled: false,
-			option: props.option
+			option: props.option,
+			overlayVisible: false
 		};
 
 		this.handleClick = this.handleClick.bind(this);
+		this.handleCancel = this.handleCancel.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
 	}
 
-
 	handleClick() {
-		this.setState({toggled: !this.state.toggled});
-		console.log(this.state.option + " " + this.state.toggled);
+		this.setState({overlayVisible: true});
+	}
+
+	handleCancel() {
+		this.setState({overlayVisible: false});
+	}
+
+	handleSubmit() {
+		this.setState({overlayVisible: false});
+		console.log("Sent:" + JSON.stringify({vote: this.state.option}));
+		const request = {
+			method: "POST",
+			headers: {
+				'Content-Type': 'application/json',
+			},
+			body: JSON.stringify({vote: this.state.option})
+		};
+		fetch('http://localhost:8080', request)
+			.then(response => response.json());
 	}
 
 	render() {
-		return <button option={this.props.option} className="option-button" onClick={this.handleClick}>
-				<img className={`image ${this.state.toggled ? "toggled" : ""}`} src={this.props.src} alt={this.props.alt}/>
-			</button>
+		return <div>
+				<button option={this.props.option} className="option-button" onClick={this.handleClick}>
+				<img className="image" src={this.props.src} alt={this.props.alt}/>
+				</button>
+				<div className={`overlay ${this.state.overlayVisible ? "" : "hidden"}`}>
+					<div className="dialog">
+						<h2>Are you sure you want to vote for {this.state.option}?</h2>
+						<div className="dialog-buttons">
+							<button className="button button--submit" onClick={this.handleSubmit}>Submit</button>
+							<button className="button" onClick={this.handleCancel}>Cancel</button>
+						</div>
+					</div>
+				</div>
+			</div>
 	}
 }
